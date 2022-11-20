6a7fe439d11d67dd3fd21630371124bf3b37a087
Luoming Zhang <lokissue@gmail.com>
2022-11-20 17:12:30 +1100

    style: global sass variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './App.scss';
+import { Header, Footer, Content } from 'components/Layout/';
+import Home from 'views/Home';
+import { BRAND_NAME } from 'constants/data';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header className={'header'}>
+        <div className={'header-wrapper'}>
+          <a href='/'>
+            <span className={'header-title'}>{BRAND_NAME}</span>
+          </a>
+        </div>
+      </Header>
+      <Content className='content'>
+        <div className={'content-wrapper'}>
+          <Home />
+        </div>
+      </Content>
+      <Footer>
+        <div className={'footer-wrapper'}>
+          <p>Made by Luoming Zhang</p>
+          <p>&reg;{` ${new Date().getFullYear()} ${BRAND_NAME}. All rights reserved.`}</p>
+        </div>
+      </Footer>
     </div>
   );
 }
